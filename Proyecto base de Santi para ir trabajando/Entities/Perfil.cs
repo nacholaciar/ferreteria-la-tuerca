c9599cc63c119c603f4beade1de680bc5062acb8
81404-Nicolas_Yoles <81490449+nicolasYoles@users.noreply.github.com>
2021-05-06 08:32:04 -0300

    actualizo branch

---
+++ Proyecto base de Santi para ir trabajando/Entities/Perfil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferreteria.Entities
+{
+
+    public class Perfil
+    {
+        private int IdPerfil;
+        private string Nombre;
+    }
+
+    public Perfil()
+    {
+
+    }
+
+    public Perfil (int idPerfil, string nombre)
+    {
+        this.IdPerfil = idPerfil;
+        this.Nombre = nombre;
+    }
+
+    public int IdPerfilUsuario
+    {
+        get => IdPerfil;
+        set => IdPerfil = Value;
+    }
+    
+    public string NombreDeUsuario
+    {
+        get => Nombre;
+        set => Nombre = value;
+    }
+}
