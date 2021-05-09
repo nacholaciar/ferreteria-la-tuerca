82ee8b5f344ee1f9d59b4231142938396bb23267
ignaciolaciar <71348@sistemas.frc.utn.edu.ar>
2021-05-09 15:24:16 -0300

    [form Registrar Proveedor] clase prov creada y datos obtenidos desde el form

--- ABMProveedor/Registrar Proveedor.cs
+++ ABMProveedor/Registrar Proveedor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ABMProveedor.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,55 @@ namespace ABMProveedor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void maskedTextBox3_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Proveedor DatosProveedor()
+        {
+            Proveedor p = new Proveedor();
+
+            // obtener texts
+            p.CUITProveedor = txtCUITP.Text.Trim();
+            p.RazonSocialProveedor = txtRazonSocialP.Text.Trim();
+            p.NombreProveedor = txtNombreP.Text.Trim();
+            p.ApellidoProveedor = txtApellidoP.Text.Trim();
+            p.TelefonoProveedor = txtTelefonoP.Text.Trim();
+            p.CalleProveedor = txtCalleP.Text.Trim();
+
+            // obtener mask
+            p.NumeroProveedor = int.Parse(mksCodBarrioP.Text.Trim());
+            p.CodBarrioProveedor = int.Parse(mskNroCasaP.Text.Trim());
+
+            return p;
         }
     }
 }
